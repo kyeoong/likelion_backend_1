d1270985379ddf7bcd887843a7a8624faa99781b
kyeoong <nogangsss@gmail.com>
2023-08-17 23:55:38 +0900

    first upload

--- django_guestbook_app-main/guestbook/urls.py
+++ django_guestbook_app-main/guestbook/urls.py
@@ -7,13 +7,6 @@ urlpatterns = [
     path('pre-order/', views.pre_order_view, name='pre_order'),
     path('silver-bot/', views.silver_bot_view, name='silver_bot'),
     path('recommend/', views.recommend_view, name='recommend'),
-]
-
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
     path('', views.index, name='index'),
     path('sign/', views.sign, name='sign')
 ]
